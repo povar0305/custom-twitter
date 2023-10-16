d59c321c2ebdb92e505f946355bb27e538190e36
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2023-10-16 19:49:19 +0300

    init

--- app.vue
+++ app.vue
@@ -1,5 +1,68 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app">
+    <h2>Личный дневник</h2>
+    <form @submit.prevent="chechForm">
+      <input
+        type="text"
+        required
+        v-model.trim="title"
+        placeholder="Заголовок заметки"
+      />
+      <textarea
+        required
+        v-model.trim="text"
+        placeholder="Текст заметки"
+      ></textarea>
+
+      <button type="submit">Записать</button>
+    </form>
+
+    <div>
+      <ul>
+        <li v-for="(post, i) of posts" :key="i">
+          {{ post }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: function () {
+    return {
+      text: "",
+      title: "",
+      posts: [
+        {
+          text: "hello",
+          title: null,
+          date: Date,
+        },
+      ],
+    };
+  },
+  methods: {
+    chechForm() {
+      console.log("input_text", this.text);
+      this.posts.push({
+        text: this.text,
+        title: this.title,
+        date: new Date(Date.now()).toLocaleString(),
+      });
+      this.text = "";
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;700&display=swap");
+
+#app {
+  font-family: "Raleway", sans-serif;
+}
+</style>
